20b51243bfc0b784c4e9fdfa3f42eb255bdd2364
kennyfrc <fxkennyfrc@gmail.com>
2021-07-14 17:50:48 +0800

    bitboard representation

---
+++ src/board.js
@@ -0,0 +1,108 @@
+const BitHelper = require('./helpers.js').BitHelper
+
+class Board {
+    constructor() {
+        this.start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        this.bb = BigInt(0)
+        this.pieceBoards = {   'k': BigInt(0), 'q': BigInt(0), 'r': BigInt(0), 'b': BigInt(0), 'n': BigInt(0), 'p': BigInt(0),
+                               'K': BigInt(0), 'Q': BigInt(0), 'R': BigInt(0), 'B': BigInt(0), 'N': BigInt(0), 'P': BigInt(0)}
+
+        this.castleStatus = 0
+        this.castleBit = { 'K': 1, 'Q': 2, 'k': 4, 'q': 8 }
+
+        this.whiteToMove = 1
+        this.moveBit = { 'w': 1, 'b': 0 }
+    }
+
+    parseFenToBoard(fen) {
+        this.resetBoard()
+
+        let fen_index = 56 // fens start at a8
+        let ranksRead = 1
+        let whiteSpace = 0
+
+        for (let i = 0; i < fen.length; i++) {
+            if (ranksRead == 8 && whiteSpace == 1) {
+                if ('wb'.includes(fen[i])) { 
+                    this.moveStatus = this.moveBit[fen[i]]
+                }
+
+                if ('KQkq'.includes(fen[i])) {
+                    this.castleStatus |= this.castleBit[fen[i]]
+                }
+            } else {
+
+                if ('kqrbnpKQRBNP'.includes(fen[i])) {
+                    this.pieceBoards[fen[i]] = BitHelper.setBit(this.pieceBoards[fen[i]], fen_index)
+                    fen_index += 1
+                }
+                
+                if ('12345678'.includes(fen[i])) {
+                    fen_index += parseInt(fen[i])
+                }
+
+                if (fen[i] == '/') {
+                    fen_index = (56 - (ranksRead * 8))
+                    ranksRead += 1
+                }
+
+                if (fen[i] == ' ') {
+                    whiteSpace += 1
+                }
+            }
+        }
+
+        Object.keys(this.pieceBoards).forEach(piece => {
+            this.bb |=  this.pieceBoards[piece]
+        })
+    }
+
+    // A8 = 56 thus, that's where we do the exclusive xor (where you flip)
+    // you might need to flip the main boards first
+    flipBoard() {
+        for (let sq = 0; sq < 32; sq++) {
+            for (let [piece, board] of Object.entries(this.pieceBoards)) {
+                let bit = BitHelper.getBit(board, sq)
+                let inverted_bit = BitHelper.getBit(board, sq ^ 56)
+                
+                board = BitHelper.updateBit(board, sq, inverted_bit)
+                board = BitHelper.updateBit(board, sq ^ 56, bit)
+
+                this.pieceBoards[piece] = board
+            }
+        }
+    }
+
+    resetBoard() {
+        this.pieceBoards = {   'k': BigInt(0), 'q': BigInt(0), 'r': BigInt(0), 'b': BigInt(0), 'n': BigInt(0), 'p': BigInt(0),
+                             'K': BigInt(0), 'Q': BigInt(0), 'R': BigInt(0), 'B': BigInt(0), 'N': BigInt(0), 'P': BigInt(0)}
+    }
+}
+
+// Least Significant File Mapping
+function squareIdx(rank_idx, file_idx) {
+    return ( 8 * rank_idx + file_idx )
+}
+
+// LERF-mapping constants
+const A_FILE = BigInt("0x0101010101010101")
+const H_FILE = BigInt("0x8080808080808080")
+const FIRST_RANK = BigInt("0x00000000000000FF")
+const EIGHTH_RANK = BigInt("0xFF00000000000000")
+const A1_H8_DIAGONAL = BigInt("0x8040201008040201")
+const H1_A8_DIAGONAL = BigInt("0x0102040810204080")
+const LIGHT_SQ = BigInt("0x55AA55AA55AA55AA")
+const DARK_SQ = BigInt("0xAA55AA55AA55AA55")
+
+// noWe         nort         noEa
+//         +7    +8    +9
+//             \  |  /
+// west    -1 <-  0 -> +1    east
+//             /  |  \
+//         -9    -8    -7
+// soWe         sout         soEa
+
+
+module.exports = { 
+    Board: Board,
+}
---
+++ test/board.js
@@ -0,0 +1,88 @@
+const assert = require('assert')
+const Engine = require('../src/board.js')
+
+const Board = require('../src/board.js').Board
+const BoardView = require('../src/boardview.js').BoardView
+const Square = require('../src/square.js').Square
+
+BigInt.prototype.toJSON = function () {
+	return this.toString() + 'n'
+}
+
+let board = new Board()
+board.parseFenToBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
+describe('Board', function() {	
+	describe('#parseFenToBoard()', function() {
+		it('returns a bitboard for a given fen', function() {
+			assert.equal(board.bb, 18446462598732906495n)
+		})
+	})
+
+	describe('#displayCastleStatus', function() {
+		it('returns the integer representing the castling status', function() {
+			assert.equal(board.castleStatus, 15)
+		})
+	})
+
+	describe('#flipBoard()', function() {
+		it('flips the board', function() {
+			let flipped_board = new Board()
+			flipped_board.parseFenToBoard("r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq ")
+			flipped_board.flipBoard()
+
+			assert.equal(flipped_board.bb, 15992005286099480479n)
+		})
+	})
+	
+	describe('#indicesFor', function() {
+		let pieceBoards = board.pieceBoards
+
+		it("shows indices of black rook", function() {
+			assert.equal(Square.indicesFor('r', pieceBoards)[0], 56)
+			assert.equal(Square.indicesFor('r', pieceBoards)[1], 63)	
+		})
+
+		it("shows indices of black king", function() {
+			assert.equal(Square.indicesFor('k', pieceBoards)[0], 60)
+		})
+
+		it("shows indices of white king", function() {
+			assert.equal(Square.indicesFor('K', pieceBoards)[0], 4)
+		})
+	})
+})
+
+describe('BoardView', function() {
+	// in binary, you have to read from right to left
+	// so you need to imagine going through the FEN in reverse order of rank and file
+	// ...b8b7b6b5b4b3b2b1a8a7a6a5a4a3a2a1
+	// with this function, this makes it easier to understand for regular people
+	describe("#display()", function() {
+		it("shows the all the pieces on the board", function() {
+			assert.equal(new BoardView(board.pieceBoards).display(), "11111111\n11111111\n00000000\n00000000\n00000000\n00000000\n11111111\n11111111")
+		})
+
+		it('correctly flips the board', function() {
+			let flipped_board = new Board()
+			flipped_board.parseFenToBoard("r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq ")
+			flipped_board.flipBoard()
+
+			assert.equal(new BoardView(flipped_board.pieceBoards).display(),  '11111001\n11110111\n00000100\n00101000\n00101000\n00100000\n11110111\n10111011')
+		})
+	})
+
+	describe('#displayPiece()', function() {
+		it('shows the correct initial pawn & piece placements', function() {
+			let pieceBoards = board.pieceBoards
+
+			assert.equal(new BoardView(pieceBoards).displayPiece('P'), '00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n11111111\n00000000')
+			assert.equal(new BoardView(pieceBoards).displayPiece('N'), '00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n01000010')
+			assert.equal(new BoardView(pieceBoards).displayPiece('B'), '00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n00100100')
+			assert.equal(new BoardView(pieceBoards).displayPiece('R'), '00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n10000001')
+			assert.equal(new BoardView(pieceBoards).displayPiece('Q'), '00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n00010000')
+			assert.equal(new BoardView(pieceBoards).displayPiece('K'), '00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n00000000\n00001000')
+		})
+	})
+
+})

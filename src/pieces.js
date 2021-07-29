03acf60c71a69fe1fa9a17358038eac5888bffa1
kennyfrc <fxkennyfrc@gmail.com>
2021-07-29 08:54:47 +0800

    refactoring - some improvement for 7a0a56e699c5019d2e46ae6efee195a6484208d5

--- src/pieces.js
+++ src/pieces.js
@@ -4,9 +4,15 @@ class Pieces {
       case 'w':
         return ['K', 'Q', 'R', 'B', 'N', 'P'];
       case 'b':
-        return ['k', 'q', 'r', 'b', 'n', 'p'];;
-      default:
+        return ['k', 'q', 'r', 'b', 'n', 'p'];
+      case undefined:
         return ['K', 'Q', 'R', 'B', 'N', 'P', 'k', 'q', 'b', 'r', 'n', 'p'];
+      default:
+        if ('KQRBNPkqrbnp'.includes(fromPieceOrSide)) {
+          return [fromPieceOrSide];
+        } else {
+          throw new Error('Invalid Piece or Side');
+        }
     }
   }
 }
